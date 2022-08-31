1aedaccc4bf8b36259b573a0d5058218484f952f
mule <belmeetmule@gmail.com>
2022-08-31 16:05:15 +0300

    configure linters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import _ from 'lodash';
-import {scores} from './modules/scores.js';
+import { scores } from './modules/scores.js';
 import './style.css';
 
 function addScore(score) {
-    const list = document.querySelector('.score-list');
-    const li = document.createElement('li');
-    li.innerHTML = score.name + ': '+ score.score;
-    list.appendChild(li);
-  }
-  
-  console.log(scores);
-scores.forEach(i => {
+  const list = document.querySelector('.score-list');
+  const li = document.createElement('li');
+  li.innerHTML = `${score.name}: ${score.score}`;
+  list.appendChild(li);
+}
+
+scores.forEach((i) => {
   addScore(i);
-}) 
+});
--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,32 +1,32 @@
-let scores = [
+const scores = [
   {
-    name:'Abebe',
-    score:100
+    name: 'Abebe',
+    score: 100,
   },
   {
-    name:'Sara',
-    score:20
+    name: 'Sara',
+    score: 20,
   },
   {
-    name:'Ayele',
-    score:50
+    name: 'Ayele',
+    score: 50,
   },
   {
-    name:'Lily',
-    score:78
+    name: 'Lily',
+    score: 78,
   },
   {
-    name:'Nael',
-    score:125
+    name: 'Nael',
+    score: 125,
   },
   {
-    name:'Frehiwot',
-    score:77
+    name: 'Frehiwot',
+    score: 77,
   },
   {
-    name:'Martha',
-    score:42
-  }
-]
-
-export {scores};
+    name: 'Martha',
+    score: 42,
+  },
+];
+// eslint-disable-next-line import/prefer-default-export
+export { scores };
