fb6544b0af5a2409a9444125d7cbc67c522eabe1
mule <belmeetmule@gmail.com>
2022-09-01 21:51:31 +0300

    add script to refresh the leadboard on click

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,12 +1,31 @@
 export default class UI {
+
+  resetBoard(){
+    const list = document.querySelector('.score-list');
+    list.innerHTML='';
+  }
   refreashBoard( game ) {
     const list = document.querySelector('.score-list');
     const li = document.createElement('li');
-    li.innerHTML = `${ game.name }: ${ game.score }`;
+    li.innerHTML = `${ game.user }: ${ game.score }`;
     list.appendChild(li);
 }
-// gets the values from the caller 
-postGames ( user, score ) {
-  return this.true;
-}
-}
+
+// get user and score from the UI
+  getUser() {
+    const add = document.getElementById('addButton');
+    const userOb = {};
+      const user = document.getElementById('name');
+      const score = document.getElementById('score');
+      if (user.value !== '' && typeof parseInt(score.value, 10) === 'number') {
+        userOb.user = user.value;
+        userOb.score = parseInt(score.value, 10);
+        user.value = '';
+        score.value = '';
+      } else {
+        this.alert('Please enter valid data');
+      }
+      return userOb;
+    }
+
+  }
