17a16090aaa91f56ba7b3db0c7b8a2f7f3f8741d
mule <belmeetmule@gmail.com>
2022-09-02 16:22:01 +0300

    add refrsh method

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -6,27 +6,27 @@ export default class UI {
 
   static refreashBoard(scoreList) {
     scoreList.forEach((s, index) => {
-    const list = document.querySelector('.score-list');
-    const li = document.createElement('li');
+      const list = document.querySelector('.score-list');
+      const li = document.createElement('li');
 
-    const rank = document.createElement('span');
-    rank.classList.add('rank');
-    const score = document.createElement('span');
-    score.classList.add('score');
-    const player = document.createElement('span');
-    player.classList.add('player');
+      const rank = document.createElement('span');
+      rank.classList.add('rank');
+      const score = document.createElement('span');
+      score.classList.add('score');
+      const player = document.createElement('span');
+      player.classList.add('player');
 
-    rank.innerHTML = index+1;
-    score.innerHTML = `${s.score}`;
-    player.innerHTML = index === 0 ? `${s.user} <i class="fas fa-trophy"></i>`:`${s.user}`;
-    //li.innerHTML = `${s.user}: ${s.score}`;
-    li.appendChild(rank);
-    li.appendChild(player);
-    li.appendChild(score);
+      rank.innerHTML = index + 1;
+      score.innerHTML = `${s.score}`;
+      player.innerHTML = index === 0 ? `${s.user} <i class="fas fa-trophy"></i>` : `${s.user}`;
+      // li.innerHTML = `${s.user}: ${s.score}`;
+      li.appendChild(rank);
+      li.appendChild(player);
+      li.appendChild(score);
 
-    list.appendChild(li);
-  } );  
-}
+      list.appendChild(li);
+    });
+  }
 
   // get user and score from the UI
   getUser() {
